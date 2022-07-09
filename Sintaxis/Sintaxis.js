0fdfa09a7b8b4a60e25801524569c60cdd0b7ab3
Yeison E. Castillo S <87321994+Yecasu@users.noreply.github.com>
2022-07-09 18:50:50 -0500

    Add files via upload

---
+++ Sintaxis/Sintaxis.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Yeison", 
+    32, 
+    true, 
+    new Date("october 29 1989"),
+    {
+        titulo: "El mundo y sus demonios",
+        autor: "Carl Sagan",
+        fecha: new Date("marzo 15 1997"),
+        url: "https://listado.mercadolibre.com.co/el-mundo-y-sus-demonios-carl-sagan"
+    }
+];
+
