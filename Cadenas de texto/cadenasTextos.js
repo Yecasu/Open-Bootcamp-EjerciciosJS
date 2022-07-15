5e19301d030f8b0d2ef5d0474eec435e149c3c3d
Yeison E. Castillo S <87321994+Yecasu@users.noreply.github.com>
2022-07-14 22:50:33 -0500

    Add files via upload

---
+++ Cadenas de texto/cadenasTextos.js
@@ -0,0 +1,20 @@
+const nombre = "Yeison"
+
+const apellido = "Castillo"
+
+const estudiante = `${nombre} ${apellido}`
+
+const estudianteMayus = estudiante.toLocaleUpperCase()
+
+const estudianteMinus = estudiante.toLocaleLowerCase()
+
+const numeroLetras = estudiante.length
+
+const primeraLetra = nombre[0]
+
+const ultimaLetra = apellido[7]
+
+const eliminarEspacios = estudiante.replace(/ /g, "")
+
+const nombreEstudiante = estudiante.includes(nombre)
+
