552e8e05e27a7bfead506d8b17f627ffa1700133
Yeison E. Castillo S <87321994+Yecasu@users.noreply.github.com>
2022-07-15 16:10:03 -0500

    Add files via upload

---
+++ Numeros/numeros.js
@@ -0,0 +1,12 @@
+const alturaCentimetros = 170;
+
+const alturaMetros = 1.70;
+
+const pesoKilogramo = 51.3;
+
+const alturaRedondeada = Math.ceil(alturaMetros)
+
+const pesoRedondeado = Math.floor(pesoKilogramo)
+
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
